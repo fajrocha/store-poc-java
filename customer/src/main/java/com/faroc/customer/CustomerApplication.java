341c2519e6a1106063c91f04529d38fbff0c7f1a
Fabio Rocha <faroc@laptop.net>
2024-03-30 21:54:13 +0000

    Removed API Gateway and Eureka. Going forward, since it will use Kubernetes, it won't need it.

--- customer/src/main/java/com/faroc/customer/CustomerApplication.java
+++ customer/src/main/java/com/faroc/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,10 +13,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                 "com.faroc.amqp"
         }
 )
-@EnableDiscoveryClient
 @EnableFeignClients(
         basePackages = "com.faroc.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
