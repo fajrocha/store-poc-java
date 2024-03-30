702b3964812623965b73b6fbc876fb9b36680758
Fabio Rocha <faroc@laptop.net>
2024-03-30 23:47:51 +0000

    Refactored clients to have its settings injected by its users. Added application-kube profiles.

--- customer/src/main/java/com/faroc/customer/CustomerApplication.java
+++ customer/src/main/java/com/faroc/customer/CustomerApplication.java
@@ -2,10 +2,7 @@ package com.faroc.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -16,9 +13,6 @@ import org.springframework.context.annotation.PropertySources;
 @EnableFeignClients(
         basePackages = "com.faroc.clients"
 )
-@PropertySources({
-        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
-})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
